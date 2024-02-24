87fae5ef05c12098821f92df8913cef8a7de7f0e
MuznaFathima <139346884+MuznaFathima@users.noreply.github.com>
2024-02-24 08:51:26 +0000

    convertion of datatypes

---
+++ 01_basics/03_convertionOperation.js
@@ -0,0 +1,22 @@
+// let score="35"
+// console.log(typeof (score));
+
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);       
+
+
+let isLoggedIn="rewgw"
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(typeof isLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
+
+
+let num=33
+let stringNumber=String(num)
+
+console.log(typeof num);
+console.log(typeof stringNumber);
+console.log(stringNumber);
