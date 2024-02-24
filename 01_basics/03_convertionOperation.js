fffa54ffc6754e443079e3bfc960932c1075ed0c
MuznaFathima <139346884+MuznaFathima@users.noreply.github.com>
2024-02-24 09:33:06 +0000

    postfix,prefix

--- 01_basics/03_convertionOperation.js
+++ 01_basics/03_convertionOperation.js
@@ -6,17 +6,76 @@
 // console.log(valueInNumber);       
 
 
-let isLoggedIn="rewgw"
+// let isLoggedIn="rewgw"
 
-let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(typeof isLoggedIn);
-console.log(typeof booleanIsLoggedIn);
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(typeof isLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 
-let num=33
-let stringNumber=String(num)
+// let num=33
+// let stringNumber=String(num)
 
-console.log(typeof num);
-console.log(typeof stringNumber);
-console.log(stringNumber);
+// console.log(typeof num);
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
+// *******************operations*******************
+
+// let value=3
+// let negvalue= -value
+// console.log(negvalue);
+
+
+// let str1='hello0'
+// let str2='welcome'
+
+// let str3=str1+str2
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log(+true);
+
+// let num1,num2,num3
+
+// num1=num2=num3=2+2
+// console.log(num1);
+
+// let counter=100
+
+
+// ++counter
+
+// console.log(counter);
+
+
+
+// prefix and postfix
+
+let x=3
+const y=x++;
+console.log(x);
+console.log(y);
+// console.log(`x${x},y${y}`);
+
+let a=3;
+const b=++a;
+
+console.log(a);
+console.log(b);
+
+
+
+let c=7
+let d=c++
+console.log(c);
+console.log(d);
+
+let e=100
+let f=++e
+console.log(e);
+console.log(f);
