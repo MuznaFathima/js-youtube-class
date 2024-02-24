b0d8875c352362c7f90736806bb46d83ab4bd4a3
MuznaFathima <139346884+MuznaFathima@users.noreply.github.com>
2024-02-24 07:15:56 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,12 @@
+// treat all js code as newer version
+// alert("hello") we are using node js not browser
+
+
+console.log(3+5);
+// number=>range 2 to power 53
+// bigint
+console.log(typeof 'muzna');
+console.log(typeof true);
+console.log(typeof 1);
+console.log(typeof null);
+console.log(typeof undefined);
