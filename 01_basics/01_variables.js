4fdb8b729d08d10873722b13335c5f484e503c43
MuznaFathima <139346884+MuznaFathima@users.noreply.github.com>
2024-02-24 07:15:35 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId=14453
+let accountEmail="muzuft@gmail.com"
+var accountPassword="12345"
+accountCity="kerala"
+var accountNo;
+
+accountEmail="sal@gmail.com"
+accountPassword='4567'
+accountCity='malappuram'
+// accountId=2
+/*
+
+prefer not to use var
+because of issue in functional scope and block scope
+ */
+console.table([accountId,accountEmail,accountPassword,accountCity,accountNo])
