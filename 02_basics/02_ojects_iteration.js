4de9f2dcd438cf76eda14f7475efcc5996331360
MuznaFathima <139346884+MuznaFathima@users.noreply.github.com>
2024-03-02 04:24:55 +0000

    iteration with for in loop

--- 02_basics/02_ojects_iteration.js
+++ 02_basics/02_ojects_iteration.js
@@ -1,5 +1,4 @@
 
-console.log('hi welcome');
 
 const person={
 
@@ -15,3 +14,25 @@ const person={
 for(let key in person){
 console.log(key);
 }
+
+for(let key in person){
+
+    console.log(person[key]);
+}
+
+// object computation
+
+const key1='mykey1'
+const key2='mykey2'
+
+const value1='myvalue1'
+const value2='myvalue2'
+
+const obj={}
+
+console.log(obj[key1]=value1);
+console.log(obj[key2]=value2);
+console.log(obj);
+
+
+
