4b245dbbcc3c5183b34010d92c11470aee8c076f
MuznaFathima <139346884+MuznaFathima@users.noreply.github.com>
2024-03-02 03:36:52 +0000

    objects

---
+++ 02_basics/02_ojects_iteration.js
@@ -0,0 +1,17 @@
+
+console.log('hi welcome');
+
+const person={
+
+
+    name:"daniya",
+    age:22,
+    "person hobbies":["coding","sleeping"]
+}
+
+
+// for in loop
+
+for(let key in person){
+console.log(key);
+}
